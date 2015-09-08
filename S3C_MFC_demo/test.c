09d238728f6e25879bfc43a5767e7a57782678c0
tianxinxue <tianxinxue@aliyun.com>
2015-09-08 15:17:03 +0800

    2015-9-8

---
+++ S3C_MFC_demo/test.c
@@ -0,0 +1,25 @@
+
+#include "encoder_test.h"
+
+int main(int argc, char **argv)
+{
+	//Test_H263_Decoder_Line_Buffer(argc, argv);
+	//Test_H264_Decoder_Line_Buffer(argc, argv);
+	//Test_MPEG4_Decoder_Line_Buffer(argc, argv);
+	//Test_VC1_Decoder_Line_Buffer(argc, argv);
+	//Test_Decoder_Ring_Buffer(argc, argv);
+	//Test_Display_H264(argc, argv);
+	//Test_Display_MPEG4(argc, argv);
+	//Test_Display_H263(argc, argv);
+	//Test_Display_VC1(argc, argv);
+	Test_H264_Encoder(argc, argv);
+	//Test_MPEG4_Encoder(argc, argv);	
+	//Test_H263_Encoder(argc, argv);
+	//Demo(argc, argv);
+	//Test_Display_Optimization1(argc, argv);
+	//Test_Display_Optimization2(argc, argv);
+
+	return 0;
+}
+
+
